89bf96dbcdd817c767bbfd2f75643251217d01f8
shankargiri <shankargiri.p@gmail.com>
2014-06-15 15:14:50 +1000

    env modified

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => getenv('ENV'),
 			'database'  => getenv('DB_PASSWORD'),
 			'username'  => getenv('DB_PASSWORD'),
 			'password'  => getenv('DB_PASSWORD'),
